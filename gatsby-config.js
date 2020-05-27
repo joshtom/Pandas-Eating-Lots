534953dece50b06c8b839e579790e53475824538
joshtom <olajidejoshua4real@gmail.com>
2020-05-27 09:05:23 +0100

    Create a list of  markdown files in index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: `${__dirname}/src/`
       },
     },
-    `gatsby-transformemer-remark`,
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
